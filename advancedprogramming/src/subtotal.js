3d5bd02d281ed603ee4a36b06c9fcba7efe4554a
Samvit Jatia <samvit@hotmail.com>
2021-10-26 19:22:54 +0530

    checkout page

--- advancedprogramming/src/subtotal.js
+++ advancedprogramming/src/subtotal.js
@@ -1,8 +1,33 @@
 import React from "react";
 import "./subtotal.css";
+import CurrencyFormat from "react-currency-format";
 
 function subtotal() {
-  return <div></div>;
+  return (
+    <div className="subtotal">
+      <CurrencyFormat
+        renderText={(value) => (
+          <>
+            <p>
+              Subtotal (0 items): <strong>0</strong>
+            </p>
+            <small>
+              <input type="checkbox" /> This order contains a gift
+            </small>
+          </>
+        )}
+        decimalScale={2}
+        value={0}
+        displayType={"text"}
+        thousandSeparator={true}
+        prefix={"$"}
+      />
+
+      <button className="button_checkout">
+        <strong>Proceed To Checkout</strong>
+      </button>
+    </div>
+  );
 }
 
 export default subtotal;
